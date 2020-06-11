31dbaa40cb3090a5cf78e60c8f9783a7230786f0
MohammadReza <md.mohammadreza+git@gmail.com>
2020-06-11 16:29:33 +0430

    import useState & set if for render other components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Normal from './components/Normal';
 import Professional from './components/Professional';
-import Result from './components/Result';
 
 const App = (props) => {
 
+    const [method, setMethod] = useState('normal');
+
     return (
-        <div className="container vh-100 d-flex align-items-center justify-content-center">
+        <div className="container vh-100 d-flex align-items-center justify-content-center" id="hava">
             <div className="form-group row w-100 justify-content-center">
                 <div className="btn-group-vertical order-sm-1 m-5 m-md-0">
-                    <button className="btn btn-dark border">روش معمولی</button>
-                    <button className="btn btn-dark border">روش حرفه‌ای</button>
+                    <button className="btn btn-dark border" onClick={setMethod.bind(this, 'normal')}>روش معمولی</button>
+                    <button className="btn btn-dark border" onClick={setMethod.bind(this, 'professional')}>روش حرفه‌ای</button>
                 </div>
-                <Normal/>
-                <Professional/>
-                <Result/>
+                {method == 'normal' ? <Normal /> : <Professional />}
             </div>
         </div>
     )
