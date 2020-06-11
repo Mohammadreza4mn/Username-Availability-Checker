c33092bbabff661613afa523b20389b5bc074516
MohammadReza <md.mohammadreza+git@gmail.com>
2020-06-11 16:45:24 +0430

    import useContext to Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GenerateContext from '../context/Generate-context';
 
 const Result = (props) => {
 
+    const getUserName = useContext(GenerateContext);
+
     return (
-        <div className="col-md-5">
-            <input type="text" className="form-control text-center my-1" />
-            <button className="btn btn-dark border btn-block">شروع مجدد</button>
-        </div>
+            <div className="col-md-5">
+                {getUserName.resultUserName.map((item) => (
+                    <input type="text" className="form-control text-center my-1" value={item} key={item} />
+                ))}
+                <button className="btn btn-dark border btn-block" onClick={getUserName.display}>شروع مجدد</button>
+            </div >
     )
 }
 
