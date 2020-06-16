a37846c6ab717b3d9aaaaa3100d74f635ffa1355
MohammadReza <md.mohammadreza+git@gmail.com>
2020-06-16 15:51:56 +0430

    update onClick

--- src/components/Result.js
+++ src/components/Result.js
@@ -10,7 +10,7 @@ const Result = (props) => {
                 {getUserName.resultUserName.map((item) => (
                     <input type="text" className="form-control text-center my-1" value={item} key={item} />
                 ))}
-                <button className="btn btn-dark border btn-block" onClick={getUserName.display}>شروع مجدد</button>
+                <button className="btn btn-dark border btn-block" onClick={getUserName.actionDisplay}>شروع مجدد</button>
             </div >
     )
 }
