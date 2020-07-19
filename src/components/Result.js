4357e3b888ce723a2cc04d803fbcf42dc2ad24c7
MohammadReza <md.mohammadreza+git@gmail.com>
2020-07-19 06:49:05 +0430

    add Availability component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,17 +1,33 @@
 import React, { useContext } from 'react';
 import GenerateContext from '../context/Generate-context';
+import Availability from './Availability';
 
 const Result = (props) => {
 
     const getUserName = useContext(GenerateContext);
 
     return (
-            <div className="col-md-5">
-                {getUserName.resultUserName.map((item) => (
-                    <input type="text" className="form-control text-center my-1" value={item} key={item} />
-                ))}
-                <button className="btn btn-dark border btn-block" onClick={getUserName.actionDisplay}>شروع مجدد</button>
-            </div >
+        <div className="col-md-10">
+            <table className="table table-dark text-center table-hover rounded">
+                <caption>
+                    <a className="badge badge-light" href="http://github.com/mohammadreza4mn">
+                        <img style={{ width: "50px" }} src="public/../assets/img/m.m.jpg" />
+                    </a>
+                </caption>
+                <thead>
+                    <tr>
+                        <th scope="col">نام کاربری پیشنهادی</th>
+                        <th scope="col" colspan="5">وبسایت های قابل ثبت نام</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getUserName.resultUserName.map((item, index) =>
+                        <Availability resultUserName={item} key={index} />
+                    )}
+                </tbody>
+            </table>
+            <button className="btn btn-dark border btn-block" onClick={getUserName.actionDisplay}>مرحله قبل</button>
+        </div >
     )
 }
 
