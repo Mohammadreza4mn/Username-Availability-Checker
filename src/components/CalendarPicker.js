91abbdaa2289706894bd72b517c7f052956b6f59
MohammadReza <md.mohammadreza+git@gmail.com>
2020-07-19 06:50:47 +0430

    update export

--- src/components/CalendarPicker.js
+++ src/components/CalendarPicker.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import DatePicker from "react-modern-calendar-datepicker";
+export let dateBirth;
 
 const CalendarPicker = () => {
-  const [selectedDay, setSelectedDay] = useState(null);
+
+  const [selectedDay, setSelectedDay] = useState();
+
+  dateBirth = selectedDay;
+
+  const renderCustomInput = ({ ref }) => (
+    <input type="text" className="form-control text-center my-2"
+      placeholder="تاریخ تولد" ref={ref}
+      value={selectedDay ? `${selectedDay.year}/${selectedDay.month}/${selectedDay.day}` : null} />
+  )
+
   return (
     <DatePicker
       value={selectedDay}
@@ -11,6 +22,9 @@ const CalendarPicker = () => {
       inputPlaceholder="Select a day"
       shouldHighlightWeekends
       locale="fa"
+      renderInput={renderCustomInput}
+      wrapperClassName="d-block"
+      calendarPopperPosition="bottom"
     />
   );
 };
